7709e84326c046bcb44fa7efa050ed8d335ad00d
Onism <119412587+OnismExplorer@users.noreply.github.com>
2024-03-16 15:06:16 +0800

    fix: Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,11 @@ function generateProgressBar() {
 }
 
 const readme = `\
-### Hi there 👋
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
 ---
 ⏰ Updated on ${new Date().toUTCString()}
 ---
+![Progress Bar CI](https://github.com/OnismExplorer/OnismExplorer/workflows/Progress%20Bar%20CI/badge.svg)\
+`
+
+console.log(readme)
